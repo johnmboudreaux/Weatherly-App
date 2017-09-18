2e0dabcc74d0b0b66b7e845d969f64fe1d1e0633
johnmboudreaux <jhnbdrx@gmail.com>
2017-09-18 09:03:58 -0600

    moved small stuff around

--- test/Welcome.test.js
+++ test/Welcome.test.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import Welcome from '../lib/Welcome.js';
+// import React from 'react';
+// import { shallow, mount } from 'enzyme';
+// import Welcome from '../lib/Welcome.js';
 
-describe('Welcome', () => {
-  it('should have a ', () => {
-    const welcome = shallow(<Welcome />);
-    const input = welcome.find('input');
-
-
-    // console.log(welcome.debug());
-    expect(welcome.state('value')).toEqual('');
-
-    const event = { target: { value: 'Baton Rouge/Denver' } };
-
-    input.simulate('change', event);
-
-    expect(welcome.state('value')).toEqual('Baton Rouge/Denver');
-  });
-});
+// describe('Welcome', () => {
+//   it('should have a ', () => {
+//     const welcome = shallow(<Welcome />);
+//     const input = welcome.find('input');
+//
+//
+//     // console.log(welcome.debug());
+//     expect(welcome.state('value')).toEqual('');
+//
+//     const event = { target: { value: 'Baton Rouge/Denver' } };
+//
+//     input.simulate('change', event);
+//
+//     expect(welcome.state('value')).toEqual('Baton Rouge/Denver');
+//   });
+// });
