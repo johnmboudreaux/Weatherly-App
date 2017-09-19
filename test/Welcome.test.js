4a33d80bc39f49b8e830efafc0badfb873b278cd
Ben Porter <bbp5280@gmail.com>
2017-09-19 16:13:10 -0600

    testing of app, finished welcome

--- test/Welcome.test.js
+++ test/Welcome.test.js
@@ -14,4 +14,15 @@ describe('Welcome', () => {
 
     expect(welcome.find('.avatar')).toBeDefined()
   });
+  it('should render an input with a class of lcation-search', () => {
+    const welcome = shallow(<Welcome />);
+
+    expect(welcome.find('.location-search')).toBeDefined()
+  });
+
+  it('should render a button with a class of sub-btn', () => {
+    const welcome = shallow(<Welcome />);
+
+    expect(welcome.find('.sub-btn')).toBeDefined()
+  });
 });
--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from '../lib/App';
+import localStorage from './mockLocalStorage';
+// import CurrentWeather from '../lib/CurrentWeather'
+
 
 
 describe('App', () => {
-    it('should render a welcome page', () => {
+  it('should render a welcome page', () => {
       const app = shallow(<App />);
 
       expect(app.find('.search-form')).toBeDefined();
     });
+
+  it('should change state for today ', () => {
+      const app = mount(<App />);
+
+      expect(app.state('today')).toEqual({});
+
+      app.setState({
+        today: { date: '11/23/1985' } });
+
+      expect(app.state('today')).toEqual({ date: '11/23/1985' });
+    });
+
+  it('should change state for ten day ', () => {
+      const app = mount(<App />);
+
+      expect(app.state('tenDay')).toEqual([]);
+
+      app.setState({
+        tenDay: [{ date: '11/23/1985' }] });
+
+      expect(app.state('tenDay')).toEqual([{ date: '11/23/1985' }]);
+    });
+
+ it('should change state current city', () => {
+      const app = mount(<App />);
+
+      expect(app.state('currentCity')).toEqual({});
+
+      app.setState({
+        currentCity: { Denver: 'Denver' } });
+
+      expect(app.state('currentCity')).toEqual({ Denver: 'Denver' });
+    });
+
+    it('should change state hourly cards', () => {
+        const app = mount(<App />);
+
+        expect(app.state('hourlyCards')).toEqual([]);
+
+        app.setState({
+          hourlyCards: [{ date: '11/23/1985' }] });
+
+        expect(app.state('hourlyCards')).toEqual([{ date: '11/23/1985' }]);
+      });
+
 });
