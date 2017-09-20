cd3501dc7372278ed57096594aae54502f02eeaa
johnmboudreaux <jhnbdrx@gmail.com>
2017-09-20 07:55:44 -0600

    beautified stuff

--- test/HourlyCards.test.js
+++ test/HourlyCards.test.js
@@ -3,7 +3,7 @@ import { shallow, mount } from 'enzyme';
 import HourlyCards from '../lib/HourlyCards';
 import App from '../lib/App';
 import HourlyCard from '../lib/HourlyCard';
-import mockData from '../test/mockData'
+import mockData from '../utils/mockData'
 
 describe('HourlyCards', () => {
   const hourlyData = mockData.hourly_forecast.splice(0, 7)
